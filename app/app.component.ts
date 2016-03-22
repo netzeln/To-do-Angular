66903c7e70e4eeab0948b7e8e04fc2dea01474a6
nic netzel and torrence stratton <netzeln@gmail.com>
2016-03-22 09:47:52 -0700

    add array and iterate through it in display

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,30 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Skeleton Angular 2 App!</h1>
+    <div class="container">
+    <h1>To-Do List</h1>
+    </div>
+    <h3 *ngFor="#task of tasks">{{task.description}}</h3>
+    </div>
   `
 })
 
 export class AppComponent{
+  public task: Task[];
+  constructor(){
+    this.task = [
+      new Task("Create To-Do LIst App", 0),
+      new Task("Learn Typescript", 1),
+      new Task("Miss PHP", 2),
+      new Task("Stay Focused", 3)
+    ];
+  }
+}
+//model class
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number){
+
+  }
 
 }
